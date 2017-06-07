3f77360e1eefbadc19042ec567eafe87c02a1f3c
thomascp2017 <pengchen@usc.edu>
2017-06-06 17:02:30 -0700

    Thomas forked from Laura and added one line
    updated HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World! Hello git!");
+		System.out.println("Thomas forked from Laura and say Hi!");
 	}
 }
