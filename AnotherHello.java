c9134a905a37c956383f0f9df48c6aaf6f9acaa2
Xiaoyu (Laura) <laurajoe310@gmail.com>
2017-06-06 17:37:01 -0700

    Laura updated AnotherHello.java

--- AnotherHello.java
+++ AnotherHello.java
@@ -1,5 +1,6 @@
 public class AnotherHello {
 	public static void main(String[] args) {
 		System.out.println("Hi Git, am I a new contribute?");
+		System.out.println("Fork: Hi Git, am I a new contribute?");
 	}
 }
